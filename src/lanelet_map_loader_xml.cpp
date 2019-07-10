5b3b7f3cab177ae1efd6769209dd1d6210b145a1
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2019-07-10 17:56:37 +0900

    remove xml implementation from lanelet map loader
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- src/lanelet_map_loader_xml.cpp
+++ src/lanelet_map_loader_xml.cpp
@@ -6,6 +6,8 @@
 
 #include <lanelet2_extension/projection/mgrs_projector.hpp>
 #include <lanelet2_extension/io/message_conversion.hpp>
+#include <lanelet2_extension/io/autoware_parser.hpp>
+
 #include <lanelet2_msgs/MapXML.h>
 #include <lanelet2_msgs/MapBin.h>
 
@@ -26,11 +28,7 @@ int main (int argc, char **argv)
 
   std::string map_path = "";
   std::string projector_type = MGRS_PROJECTOR;
-  lanelet2_msgs::MapXML map_xml_msg;
   lanelet2_msgs::MapBin map_bin_msg;
-
-  double origin_latitude = 0;
-  double origin_longitude = 0;
 
   if(!private_rosnode.hasParam("map_file"))
   {
@@ -46,7 +44,6 @@ int main (int argc, char **argv)
   if(projector_type == MGRS_PROJECTOR)
   {
     projector_ptr = std::make_shared<lanelet::projection::MGRSProjector>();
-    map_xml_msg.projector_type = lanelet2_msgs::MapXML::MGRS;
   }
   else if(projector_type == UTM_PROJECTOR)
   {
@@ -55,31 +52,27 @@ int main (int argc, char **argv)
       ROS_FATAL_STREAM("You must specify origin_latitude and origin_longitude parameter in order to use utm projector");
       return 1;
     }
+    double origin_latitude = 0;
+    double origin_longitude = 0;
     private_rosnode.param<double>("origin_latitude", origin_latitude, origin_latitude);
     private_rosnode.param<double>("origin_longitude", origin_longitude, origin_longitude);
     projector_ptr = std::make_shared<lanelet::projection::UtmProjector>(lanelet::Origin({origin_latitude, origin_longitude}));
-    map_xml_msg.projector_type = lanelet2_msgs::MapXML::UTM;
-    map_xml_msg.origin_lat = origin_latitude;
-    map_xml_msg.origin_lon = origin_longitude;
   }
   else
   {
     ROS_ERROR_STREAM( "Projector " << projector_type << "is not supported." << std::endl
                    << "Using MGRS projector instead." << std::endl);
     projector_ptr = std::make_shared<lanelet::projection::MGRSProjector>();
-    map_xml_msg.projector_type = lanelet2_msgs::MapXML::MGRS;
   }
 
   lanelet::ErrorMessages errors;
   lanelet::LaneletMapPtr map = load(map_path, *projector_ptr, &errors);
 
   // publisher to visualise lanelet elements within rviz
-  ros::Publisher map_xml_pub = rosnode.advertise<lanelet2_msgs::MapXML>("/lanelet_map_xml", 1, true);
-  ros::Publisher map_bin_pub = rosnode.advertise<lanelet2_msgs::MapBin>("/lanelet_map_bin", 1, true);
+  ros::Publisher map_bin_pub = rosnode.advertise<lanelet2_msgs::MapBin>("/lanelet_map", 1, true);
 
-  int status = lanelet_utils::Map::toXMLMsg(map, map_xml_msg, *projector_ptr);
   lanelet_utils::Map::toBinMsg(map, map_bin_msg);
-  map_xml_pub.publish(map_xml_msg);
+  lanelet::io_handlers::AutowareOsmParser::parseVersions(map_path, map_bin_msg.format_version, map_bin_msg.map_version);
   map_bin_pub.publish(map_bin_msg);
 
   ros::spin();
