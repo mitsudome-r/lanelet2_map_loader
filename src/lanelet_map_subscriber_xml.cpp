1807e966062f4adab4927c8ba6ac3e93c55f64aa
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2019-07-10 18:20:47 +0900

    remove xml implementation from lanelet_map_subscriber
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- src/lanelet_map_subscriber_xml.cpp
+++ src/lanelet_map_subscriber_xml.cpp
@@ -14,7 +14,6 @@
 #include <lanelet2_extension/projection/mgrs_projector.hpp>
 #include <lanelet2_extension/io/message_conversion.hpp>
 
-#include <lanelet2_msgs/MapXML.h>
 #include <lanelet2_msgs/MapBin.h>
 
 #include <iostream>
@@ -22,23 +21,11 @@
 #include <string>
 
 lanelet::LaneletMapPtr lanelet_map;
-lanelet::LaneletMapPtr lanelet_map2;
-
-void xmlMapCallback(lanelet2_msgs::MapXML msg)
-{
-  int status = lanelet_utils::Map::fromXMLMsg(msg, lanelet_map);
-  std::cout << status << std::endl;
-  // for (auto lanelet: lanelet_map->laneletLayer)
-  // {
-  //   std::cout << lanelet << std::endl;
-  // }
-
-}
 
 void binMapCallback(lanelet2_msgs::MapBin msg)
 {
-  lanelet_utils::Map::fromBinMsg(msg, lanelet_map2);
-  for (auto lanelet: lanelet_map2->laneletLayer)
+  lanelet_utils::Map::fromBinMsg(msg, lanelet_map);
+  for (auto lanelet: lanelet_map->laneletLayer)
   {
     std::cout << lanelet << std::endl;
   }
@@ -55,7 +42,6 @@ int main (int argc, char **argv)
   ros::init(argc, argv, "lanelet_map_subscriber");
   ros::NodeHandle rosnode;
 
-  ros::Subscriber xml_map_sub = rosnode.subscribe("/lanelet_map_xml", 10000,  xmlMapCallback);
   ros::Subscriber bin_map_sub = rosnode.subscribe("/lanelet_map_bin", 10000,  binMapCallback);
 
   ros::spin();
